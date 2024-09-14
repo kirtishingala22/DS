2d678a83cbb0db42da54faedf034608a4b473c7b
kirtishingala22 <kirti.shingala123024@marwadiuniversity.ac.in>
2024-09-14 21:41:19 +0530

    Create swap_nodes_in pair_in_linkedlist.cpp

---
+++ 100dayschallange/swap_nodes_in pair_in_linkedlist.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+         ListNode temp(0);
+    temp.next = head;
+    ListNode* prev = &temp;
+
+    while (prev->next && prev->next->next) {
+        ListNode* first = prev->next;
+        ListNode* second = prev->next->next;
+        
+        first->next = second->next;
+        second->next = first;
+        prev->next = second;
+
+        prev = first;
+    }
+    
+    return temp.next;
+    }
+};
