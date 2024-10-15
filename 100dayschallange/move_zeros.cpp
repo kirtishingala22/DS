ad8804807f6390751cd0eb0bf6548656b61e4680
kirtishingala22 <kirti.shingala123024@marwadiuniversity.ac.in>
2024-10-15 20:43:38 +0530

    Create move_zeros.cpp

---
+++ 100dayschallange/move_zeros.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> arr(n, 0); 
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                arr[j] = nums[i];
+                j++;
+            }
+        }
+
+       
+        for (int i = 0; i < n; i++) {
+            nums[i] = arr[i];
+        }
+    }
+    
+};
